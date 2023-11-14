9286bb6c9ad6d13f9f1cd9b99c3ee2e17c0c2ae3
nafismahamudshahin <nafismahamudshahin@gmail.com>
2023-11-15 01:00:10 +0600

    deposit calculation complated

---
+++ deposit.js
@@ -0,0 +1,21 @@
+document.getElementById("deposit-btn").addEventListener('click',function(){
+    const newDepositElement = document.getElementById("deposit");
+    const newDeposit =parseFloat(newDepositElement.value);
+    newDepositElement.value ='';
+    if(isNaN(newDeposit)){
+        alert("Enter valid input")
+        return
+    }
+    // deposit mony area:
+    const previousDepositElement = document.getElementById("deposit-balance");
+    const previousDepositBalance = parseFloat(previousDepositElement.innerText);
+    const totalDepositBalance = previousDepositBalance + newDeposit;
+    previousDepositElement.innerText= totalDepositBalance;
+
+    // Total Balance area updated for new deposit:
+    const totalBalanceElement = document.getElementById("total-balance");
+    const totalBalance = parseFloat(totalBalanceElement.innerText);
+    const updatedBalance = totalBalance + newDeposit;
+    totalBalanceElement.innerText=updatedBalance;
+
+})
